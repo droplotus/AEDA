557bcb6ef30eef95d89998a86e431aeb4a9cadc7
Droplotus <joaobpedrof@gmail.com>
2021-02-26 15:14:07 +0100

    adding helloworld

---
+++ 2nd semester/LPOO/helloworld/src/Application.java
@@ -0,0 +1,6 @@
+public class Application {
+    public static void main(String args[]) {
+        System.out.println("Thank you so much for using our app!");
+        System.out.println("Version 1.0");
+    }
+}
