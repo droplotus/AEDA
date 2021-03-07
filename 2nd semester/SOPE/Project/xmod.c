5ea3a4e17eee0528f798c29ba3b3c3fe3d93f532
droplotus <joaobpedrof@gmail.com>
2021-03-07 16:31:53 +0100

    upgraded the syntax error checking

--- 2nd semester/SOPE/Project/xmod.c
+++ 2nd semester/SOPE/Project/xmod.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <fcntl.h> // for open
 #include <unistd.h> // for close
+#include <stdbool.h>
 
 struct InitialData {
 	char OPTION;
@@ -12,24 +13,47 @@ struct InitialData {
 
 };
 
-int main(int argc, char **args, char **argv)
-{
-	if(argc != 4) { fprintf(stderr, "ERROR: misuse of arguments. [xmod OPTIONS MODE FILE/DIR]\n"); exit(1); }
+bool checkingSyntax(int argc, char **args, struct InitialData* data){
+	if(argc != 4) { fprintf(stderr, "ERROR: misuse of arguments. [xmod OPTIONS MODE FILE/DIR]\n"); return false; }
 	
-	char option = args[1][1];
-	char *mode = args[2];
-	char *file_dir = args[3];
+	data->OPTION = args[1][1];
+	data->MODE = args[2];
+	data->FILE_DIR = args[3];
 
-	int fd = open(args[3], O_CREAT, S_IRUSR); // open with read mode, otherwise it will give error trying to open it if it already exists
+	int fd = open(data->FILE_DIR, O_CREAT, S_IRUSR); // open with read mode, otherwise it will give error trying to open it if it already exists
 
 	// checking basic command syntax. [mode] to be developped
-	if(strlen(args[1]) != 2 || (option != 'v' && option != 'c' && option != 'R')) { fprintf(stderr, "ERROR: misuse of options. [-v, -c, -R]\n"); exit(1); }
-	if((mode[0] != 'u' && mode[0] != 'g' && mode[0] != 'o' && mode[0] != 'a')) { fprintf(stderr, "ERROR: misuse of mode. [u | g | o | a]\n"); exit(1); }
-	if(fd < 0) { fprintf(stderr, "ERROR: path of file.\n"); exit(1); }
+	if(strlen(args[1]) != 2 || (data->OPTION != 'v' && data->OPTION != 'c' && data->OPTION != 'R')) { fprintf(stderr, "ERROR: misuse of options. [-v, -c, -R]\n"); return false; }
+
+	if((data->MODE[0] != 'u' && data->MODE[0] != 'g' && data->MODE[0] != 'o' && data->MODE[0] != 'a') || !(strlen(data->MODE) <= 5 && strlen(data->MODE) >= 3)) { 
+		fprintf(stderr, "ERROR: misuse of mode. [ u | g | o | a ][ - | + | = ][ rwx ]\n"); return false; }
+
+	if((data->MODE[1] != '-' && data->MODE[1] != '+' && data->MODE[1] != '=')) {
+		fprintf(stderr, "ERROR: misuse of mode. [ u | g | o | a ][ - | + | = ][ rwx ]\n"); return false; }
+
+	char *rwx = malloc(3 * sizeof(char));
+	int c = 2;
+	while(c < strlen(data->MODE)){
+		rwx[c-2] = data->MODE[c];
+		c++;
+	}
+	if(strcmp(rwx, "r") && strcmp(rwx, "w") && strcmp(rwx, "x") && strcmp(rwx, "rw") && strcmp(rwx, "rx") && strcmp(rwx, "wx") && strcmp(rwx, "rwx")){
+		fprintf(stderr, "ERROR: misuse of mode. [ u | g | o | a ][ - | + | = ][ rwx ]\n");
+		return false;
+	}
 	
-	// filling the data struct
-	struct InitialData data = {.OPTION = option, .MODE = mode, .FILE_DIR = file_dir};
+	if(fd < 0) { fprintf(stderr, "ERROR: path of file.\n"); return false; }
 
 	close(fd);
+	return true;
+}
+
+int main(int argc, char **args, char **argv)
+{
+	struct InitialData data;
+	if(!checkingSyntax(argc, args, &data)) exit(1);
+	
+	printf("%c\n", data.OPTION);
+
 	return 0;
 }
